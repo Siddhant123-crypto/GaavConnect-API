312c255eabdd61b26c5fbb2350242993260f71f1
apoorvabari <apoorvabari16@gmail.com>
2026-08-21 11:16:38 +0530

    refactor: remove errors field from ApiResponse

--- src/utils/apiResponse.js
+++ src/utils/apiResponse.js
@@ -4,16 +4,14 @@ class ApiResponse {
     static success(res, { statusCode = 200, message = 'Success' }) {
         return res.status(statusCode).json({
             success: true,
-            message,
-            errors: null
+            message
         });
     }
 
-    static error(res, { statusCode = 500, message = 'Internal Server Error', errors = null }) {
+    static error(res, { statusCode = 500, message = 'Internal Server Error' }) {
         return res.status(statusCode).json({
             success: false,
-            message,
-            errors
+            message
         });
     }
 }
