3d5d40edc071a5e593675c1c8db8407298c23f6c
siddhant <thoratsiddhant48@gmail.com>
2026-08-21 12:58:11 +0530

    feat: separate register routes for user and sarpanch

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,8 +5,17 @@ const authController                  = require('../controllers/authController')
 const { registerValidator,
         loginValidator }              = require('../validators/authValidator');
 
-// POST /api/auth/register
-router.post('/register', registerValidator, authController.register);
+// POST /api/auth/user/register
+router.post('/user/register', (req, res, next) => {
+    req.body.userType = 'normal';
+    next();
+}, registerValidator, authController.register);
+
+// POST /api/auth/sarpanch/register
+router.post('/sarpanch/register', (req, res, next) => {
+    req.body.userType = 'sarpanch';
+    next();
+}, registerValidator, authController.register);
 
 // POST /api/auth/login
 router.post('/login', loginValidator, authController.login);
